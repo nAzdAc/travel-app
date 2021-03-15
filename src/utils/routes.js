b36d73fa82745404f7152da8842628774ee9498d
nAzdAc <nAzdAc15071996@mail.ru>
2021-03-15 11:33:01 +0300

    feat: add login, logout

---
+++ src/utils/routes.js
@@ -0,0 +1,8 @@
+const origin = 'https://travel-best-app.herokuapp.com';
+
+const getRoute = (trailing) => `${origin}/${trailing}`;
+
+export const routes = {
+  register: getRoute('register'),
+  login: getRoute('login'),
+};
