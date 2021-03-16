7ac39de38106536faa402aa807b64b24de2428a0
nAzdAc <nAzdAc15071996@mail.ru>
2021-03-16 07:02:38 +0300

    feat: add rating

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,6 +3,7 @@ const origin = 'https://travel-best-app.herokuapp.com';
 const getRoute = (trailing) => `${origin}/${trailing}`;
 
 export const routes = {
-  register: getRoute('register'),
-  login: getRoute('login'),
+	register: getRoute('register'),
+	login: getRoute('login'),
+	rating: getRoute('rating'),
 };
