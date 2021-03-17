29be1e5710823dc5a2d2db8c8d20b91989b1a511
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-03-17 18:48:52 +0300

    feat(coutryPage): feat fetch country data

--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState, useCallback, useContext } from 'react';
 import styled from 'styled-components';
 import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
+import { HeaderCountry } from '../components/HeaderCountry';
 import { ImageLarge } from '../components/ImageLarge';
 import { H1 } from '../components/H1';
 import { TextMedium } from '../components/TextMedium';
 import { TextColor } from '../components/TextColor';
 import data from '../data';
-import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
 import { useHttp } from '../hooks/http.hook';
-import { YMaps,Map } from 'react-yandex-maps';
-import '../../src/map.css';
 import {routes} from '../utils/routes'
 import { useParams } from 'react-router-dom';
+import { H2 } from '../components/H2';
+import { YMaps,Map } from 'react-yandex-maps';
+import '../../src/map.css';
 
 const CountryStyled = styled.div``;
 const RatingWrapperStyled = styled.div`display: flex;`;
@@ -21,6 +21,10 @@ const AddInfoWrapperStyled = styled.div`
 	width: 100%;
 	display: flex;
 	justify-content: space-between;
+
+  @media (max-width: 1300px) {
+    flex-direction: column;
+  }
 `;
 const YMapsStyled = styled.div`
 	width: 100%;
@@ -31,21 +35,24 @@ const YMapsStyled = styled.div`
 const WeatherWrapperStyled = styled.div`
 	display: flex;
 	flex-direction: column;
+  margin-bottom: 10px;
 `;
 
-const getResource = async (url) => {
-	const res = await axios.get(`${url}`);
-	return res.data;
-};
+const RateStyled = styled.p`
+	font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+  color: #FF6B35;
+  margin: 5px 0;
+`;
 
-const getWeatherApi = (cityName) =>
-	`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d24b900fdfd48da2c4f4dcae7fc4fb18`;
-
-const getWeatherIcon = (name) => `http://openweathermap.org/img/w/${name}.png`;
+const apiKey = "5ae2e3f221c38a28845f05b6d03a8c16da44b986d76a13df718bebe0";
 
 export const CountryPage = (props) => {
 	const {name} = useParams();
 	const [ countryWeather, setCountryWeather ] = useState({});
+  const [ countryRate, setCountryRate ] = useState({});
+	const [countryMainInfo, setCountryMainInfo] = useState({});
 	const { loading, request } = useHttp();
 	const { token } = useContext(AuthContext)
 
@@ -53,43 +60,54 @@ export const CountryPage = (props) => {
 	const countryTitle = `${countryData.name}, ${countryData.capital}`;
 	const countryDescription = countryData.description;
 
-	// useEffect(() => {
-	// 	let mounted = true;
-	// 	console.log(countryData)
-	// 	getResource(getWeatherApi(countryData.capital)).then((data) => {
-	// 		if (mounted) {
-	// 			setCountryWeather((prev) => ({ ...prev, data }));
-	// 			console.log(countryWeather)
-	// 			console.log(data)
-	// 		}
-	// 		console.log(countryWeather)
-	// 	});
-	// 	return () => (mounted = false);
-	// }, []);
-  
+	let lon = 0;
+	let lat = 0;
+
   const fetchWeather = useCallback(
 		async () => {
-      console.log('feactWeather')
 			try {
-				const data = await request(`${routes.country}?country=${countryData.name}&capital=${countryData.capital}`, 'GET', null, {
+				const data = await request(`${routes.country}?country=${countryData.name}&capital=${countryData.capital}&currencyCode=${countryData.currency}`, 'GET', null, {
 					Authorization: `Bearer ${token}`
 				});
-				console.log(data)
-				const {wind, pressure, temperature, humidity, altText, icon } = data.weather
-				console.log(data.weather)
-				setCountryWeather(data)
+				setCountryWeather(data.weather);
+        setCountryRate(data.currency);
 			} catch (e) {}
 		},
 		[ token, request ]
 	);
+	const fetchCountriMainInfo = async () => {
+    return await fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+      .then(response => response.json())
+      .then(data => {
+				setCountryMainInfo(data[0]) 
+        console.log(countryMainInfo)
+       });}
+
+	
+
+	const fetchCapitalCoordinate = async () => {
+		apiGet("geoname", "name=" + countryMainInfo.capital).then(function(data) {
+			let message = "Name not found";
+			if (data.status === "OK") {
+				lon = data.lon;
+				lat = data.lat;
+				console.log(data)
+			}
+			// firstLoad()
+		});
+	}
 
   useEffect(() => {
+		fetchCountriMainInfo()
     fetchWeather()
-  }, [fetchWeather])
+		fetchCapitalCoordinate()
+		
+
+  }, [fetchWeather, name])
 
 	return (
 		<CountryStyled>
-			<Header />
+			<HeaderCountry />
 			<ImageLarge url={countryData.imageUrl} />
 			<RatingWrapperStyled>
 				<H1 text={countryTitle} />
@@ -99,27 +117,48 @@ export const CountryPage = (props) => {
 			<AddInfoWrapperStyled>
 				<WeatherWrapperStyled>
 					<div>
-						{/* <img alt={countryWeather.weather[0].main} src={getWeatherIcon(countryWeather.weather[0].icon)} /> */}
+						<img alt={countryWeather.altText} src={countryWeather.icon} />
 					</div>
-					{/* <TextColor text={`Temp: ${countryWeather.main.temp} °F`} /> */}
-					{/* <TextColor text={`Wind: ${countryWeather.wind.speed} mph`} /> */}
+					<TextColor text={countryWeather.temperature} />
+					<TextColor text={countryWeather.wind} />
 				</WeatherWrapperStyled>
 				<WeatherWrapperStyled>
-					<TextColor text="0.84 EUR" />
-					<TextMedium text="for 1 USD" />
+					<RateStyled>{countryRate.BYN}</RateStyled>
+					<RateStyled>{countryRate.EUR}</RateStyled>
+          <RateStyled>{countryRate.USD}</RateStyled>
 				</WeatherWrapperStyled>
 				<WeatherWrapperStyled>
 					<TextColor text={new Date().toLocaleTimeString()} />
 					<TextColor text={new Date().toLocaleDateString()} />
-					<TextMedium text={countryData.capital} />
+          <RateStyled>{countryData.capital}</RateStyled>
 				</WeatherWrapperStyled>
 			</AddInfoWrapperStyled>
+			<H2 text='Расположение'></H2>
 			<YMaps>
-   			<div className="map-conteiner">
-				 <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
-				</div>
-  		</YMaps>
+      <div className="map-conteiner">
+        <Map className="map" defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+      </div>
+    </YMaps>
 			<Footer />
 		</CountryStyled>
 	);
 };
+
+function apiGet(method, query) {
+	return new Promise(function(resolve, reject) {
+		var otmAPI =
+			"https://api.opentripmap.com/0.1/en/places/" +
+			method +
+			"?apikey=" +
+			apiKey;
+		if (query !== undefined) {
+			otmAPI += "&" + query;
+		}
+		fetch(otmAPI)
+			.then(response => response.json())
+			.then(data => resolve(data))
+			.catch(function(err) {
+				console.log("Fetch Error :-S", err);
+			});
+	});
+}
