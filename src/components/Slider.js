5c4b85ab90987bfbb1d3a6cac0d4b180b8a75324
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-03-19 08:41:55 +0300

    fix(slider): fix text

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -15,7 +15,7 @@ export const SimpleSlider = ({ attractions }) => {
       slidesToScroll: 2
     }
     const listItems = attractions.map((item,index) =>
-      <Slide key={index} attraction={item.name} img={item.preview? item.preview.source: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/57/Ukraina.JPG/300px-Ukraina.JPG'} text={item.wikipedia_extracts.text}></Slide>
+      <Slide key={index} attraction={item.name} img={item.preview? item.preview.source: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/57/Ukraina.JPG/300px-Ukraina.JPG'} text={item.wikipedia_extracts.text? item.wikipedia_extracts.text : 'Kiesler and Goldberg analyzed a variety of response measures that were typically utilized as measures of attraction and extracted two factors: the first, characterized as primarily socioemotional, included variables such as liking, the desirability of the persons inclusion in social clubs and parties, seating choices, and lunching together. The second factor included variables such as voting for, admiration and respect for, and also seeking the opinion of the target.[1] Another widely used measurement technique scales verbal responses expressed as subjective ratings or judgments of the person of interes'}></Slide>
     );
 
     return (
